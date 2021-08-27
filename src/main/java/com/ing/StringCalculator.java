44fcdf033ed7cb8aef1828e4ef530e10a252028c
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-28 01:53:37 +0200

    Create the test for an empty numbers

--- src/main/java/com/ing/StringCalculator.java
+++ src/main/java/com/ing/StringCalculator.java
@@ -1,4 +1,7 @@
 package com.ing;
 
 public class StringCalculator {
+    public int add(String numbers) {
+        return 0;
+    }
 }
--- src/test/java/com/ing/StringCalculatorTest.java
+++ src/test/java/com/ing/StringCalculatorTest.java
@@ -1,12 +1,27 @@
 package com.ing;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
 
+    private StringCalculator sut;
+
     @BeforeEach
     public void setup() {
+        sut = new StringCalculator();
+    }
 
+    @Test
+    public void whenNumbersIsEmpty_ThenIsZero() {
+        final String numbers = "";
+        final int expected = 0;
+
+        int result = sut.add(numbers);
+
+        assertEquals(expected, result);
     }
 
 }
