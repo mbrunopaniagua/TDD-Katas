7422e08007055c0df83488be18637bfcd63cf028
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-28 16:05:37 +0200

    Refactor to lambda function

--- src/main/java/com/ing/StringCalculator.java
+++ src/main/java/com/ing/StringCalculator.java
@@ -1,12 +1,14 @@
 package com.ing;
 
+import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
 
 public class StringCalculator {
+    private final ToIntFunction<String> lambdaFromStringToInt = (n) -> "".equals(n) ? 0 : Integer.parseInt(n);
 
     public int add(String numbers) {
         return Stream.of(numbers.split(","))
-                .mapToInt(n -> "".equals(n) ? 0 : Integer.parseInt(n))
+                .mapToInt(lambdaFromStringToInt)
                 .sum();
     }
 }
