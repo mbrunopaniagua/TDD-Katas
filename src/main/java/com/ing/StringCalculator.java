5b6070b8f6ef96ebcc4ad5bfd4990e62bf55f401
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-29 01:58:37 +0200

    Negative value is not allowed, throws exception

--- src/main/java/com/ing/StringCalculator.java
+++ src/main/java/com/ing/StringCalculator.java
@@ -1,14 +1,20 @@
 package com.ing;
 
+import java.util.function.IntPredicate;
 import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
 
 public class StringCalculator {
-    private final ToIntFunction<String> lambdaFromStringToInt = (n) -> "".equals(n) ? 0 : Integer.parseInt(n);
+    private final ToIntFunction<String> lambdaFromStringToInt = n -> "".equals(n) ? 0 : Integer.parseInt(n);
+    private final IntPredicate lambdaFilter = n -> {
+      if (n < 0) throw new IllegalArgumentException("Negatives not allowed: " + n);
+      return true;
+    };
 
     public int add(String numbers) {
         return Stream.of(numbers.split("[,\n]"))
                 .mapToInt(lambdaFromStringToInt)
+                .filter(lambdaFilter)
                 .sum();
     }
 }
--- src/test/java/com/ing/StringCalculatorTest.java
+++ src/test/java/com/ing/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package com.ing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class StringCalculatorTest {
@@ -19,7 +23,7 @@ class StringCalculatorTest {
         sut = new StringCalculator();
     }
 
-    @ParameterizedTest(name = "#{index} - Sum {0} = {1}")
+    @ParameterizedTest(name = "#{index} - Sum \"{0}\" = {1}")
     @MethodSource("argumentsNumbersToInt")
     public void numbersToInt(String numbers, int expected) {
         int result = sut.add(numbers);
@@ -41,6 +45,14 @@ class StringCalculatorTest {
                 arguments("1\n2,3", 6),
                 arguments("1,2,3,4,5,6,7,8,9,10", 55)
         );
+    }
+
+    @Test
+    public void whenNumbersHasANegativeNumber_ThenThrowsException() {
+        final String numbers = "-1";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> sut.add(numbers));
+        assertTrue(exception.getMessage().contains("Negatives not allowed: " + numbers));
     }
 
 }
