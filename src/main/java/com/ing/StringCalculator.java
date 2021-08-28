de9f3cef2eaf37bd3244e2fffe77b152ccc066b3
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-28 14:02:06 +0200

    Fix exception and apply reduce function in functional way

--- src/main/java/com/ing/StringCalculator.java
+++ src/main/java/com/ing/StringCalculator.java
@@ -1,12 +1,15 @@
 package com.ing;
 
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     public int add(String numbers) {
         if ("".equals(numbers)) return 0;
         if (numbers.contains(",")) {
-            String[] numbersSplit = numbers.split(",");
-            return Integer.parseInt(numbersSplit[0]) + Integer.parseInt(numbersSplit[1]) + Integer.parseInt(numbersSplit[2]);
+            return Stream.of(numbers.split(","))
+                    .mapToInt(i -> Integer.parseInt(i))
+                    .sum();
         }
         return Integer.parseInt(numbers);
     }
