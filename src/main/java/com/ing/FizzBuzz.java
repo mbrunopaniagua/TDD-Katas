32f5db5ad093c5432bda7d012d415ad76d59fd35
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-30 00:28:03 +0200

    Setup FizzBuzz

---
+++ src/main/java/com/ing/FizzBuzz.java
@@ -0,0 +1,10 @@
+package com.ing;
+
+import java.util.List;
+
+public class FizzBuzz {
+
+    List<String> calculate(List<String> numbers) {
+        return null;
+    }
+}
---
+++ src/test/java/com/ing/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.ing;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class FizzBuzzTest {
+
+    private FizzBuzz sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new FizzBuzz();
+    }
+}
