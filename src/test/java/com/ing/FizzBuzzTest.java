557505854851454a8a061a96005ce2bc9a677d67
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-30 00:35:20 +0200

    Create simplest test for FizzBuzz

--- src/test/java/com/ing/FizzBuzzTest.java
+++ src/test/java/com/ing/FizzBuzzTest.java
@@ -1,6 +1,12 @@
 package com.ing;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
@@ -9,5 +15,15 @@ class FizzBuzzTest {
     @BeforeEach
     void setUp() {
         sut = new FizzBuzz();
+    }
+
+    @Test
+    public void whenInputNumbersHaveJustOneAndTwo_ThenReturnsTheSameList() {
+        final List<String> numbers = Arrays.asList("1","2");
+        final List<String> expected = Arrays.asList("1","2");
+
+        List<String> fizzBuzzList = sut.calculate(numbers);
+
+        assertEquals(expected, fizzBuzzList);
     }
 }
