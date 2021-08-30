ffcdbfe8d96f4ec5d2da3aa1553472fe51e1a48e
Mario Bruno <mario.bruno.paniagua@ing.com>
2021-08-31 01:05:29 +0200

    Add DNA pipeline specs and create set up

---
+++ src/test/java/com/ing/DNAPipelineTest.java
@@ -0,0 +1,13 @@
+package com.ing;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class DNAPipelineTest {
+
+    private DNAPipeline sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new DNAPipeline();
+    }
+}
