fba7b29966bdc3945f83a38785ba4fe061bdbaa3
Mario Bruno Paniagua <mario.bruno.paniagua@ing.com>
2022-08-18 16:56:02 +0200

    First test: instantiate a new class

---
+++ src/test/java/com/ing/FizzBuzz2Test.java
@@ -0,0 +1,14 @@
+package com.ing;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzz2Test {
+    @Test
+    public void testSomething() {
+        new FizzBuzz2();
+    }
+    
+    private class FizzBuzz2 {
+        
+    }
+}
