c917b830a3ec914a90e99f3cce09d352538e92fd
Mario Bruno Paniagua <mario.bruno.paniagua@ing.com>
2022-08-18 17:09:51 +0200

    Refactor: remove duplicated test

--- src/test/java/com/ing/FizzBuzz2Test.java
+++ src/test/java/com/ing/FizzBuzz2Test.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzz2Test {
     @Test
-    public void canInstantiate() {
-        new FizzBuzz2();
-    }
-
-    @Test
     public void respondsToGenerateMethod() {
         FizzBuzz2 fizzBuzz = new FizzBuzz2();
         fizzBuzz.generate();
