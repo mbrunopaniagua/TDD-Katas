0422a9c677941c5f96c55559b1d032330d71c3f9
Mario Bruno Paniagua <mario.bruno.paniagua@ing.com>
2022-08-18 20:48:13 +0200

    Refactor fori by stream

--- src/main/java/com/ing/FizzBuzz2.java
+++ src/main/java/com/ing/FizzBuzz2.java
@@ -1,33 +1,35 @@
 package com.ing;
 
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzz2 {
 
     private static final int NUM_OF_ELEMENTS = 100;
+    private static final Map<Predicate<Integer>, String> replacementsRules = new LinkedHashMap<>();
+    static {
+        replacementsRules.put(number -> number % 15 == 0, "FizzBuzz");
+        replacementsRules.put(number -> number % 3 == 0, "Fizz");
+        replacementsRules.put(number -> number % 5 == 0, "Buzz");
+    }
 
     public List<String> generate() {
-        List<String> list = new ArrayList<>(NUM_OF_ELEMENTS);
-        for (int i = 0; i < NUM_OF_ELEMENTS; i++) {
-            int number = i+1; 
-            String element = String.valueOf(number);
-            
-            if (number % 3 == 0) {
-                element = "Fizz";
-            }
-
-            if (number % 5 == 0) {
-                element = "Buzz";
-            }
-
-            if (number % 15 == 0) {
-                element = "FizzBuzz"; 
-            }
-
-            list.add(i, element);
-        }
-        return list;
+        return IntStream.rangeClosed(1, NUM_OF_ELEMENTS)
+        .boxed()
+        .map(this::calculate)
+        .collect(Collectors.toList());
+    }
+    
+    private String calculate(int number) {
+        return replacementsRules.keySet().stream()
+            .filter(rule -> rule.test(number))
+            .findFirst()
+            .map(rule -> replacementsRules.get(rule))
+            .orElse(String.valueOf(number));
     }
     
 }
